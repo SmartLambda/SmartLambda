4a96c7d07310b315cbd0e60d65976a26c15d4f04
Johannes <ubezl@student.kit.edu>
2017-01-27 14:31:34 +0100

    Added first basic main class

---
+++ src/main/java/edu/teco/smartlambda/Application.java
@@ -0,0 +1,14 @@
+package edu.teco.smartlambda;
+
+/**
+ *
+ */
+public class Application {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String... args) {
+		System.out.println("Hello Smart World.");
+	}
+}
