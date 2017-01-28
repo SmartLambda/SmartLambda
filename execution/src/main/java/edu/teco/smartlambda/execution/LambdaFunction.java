a9e5e9a8622f26587043a569af06835597d007d2
Johannes Hengstler <ubezl@student.kit.edu>
2017-01-28 16:13:09 +0100

    Added execution stub classes

---
+++ execution/src/main/java/edu/teco/smartlambda/execution/LambdaFunction.java
@@ -0,0 +1,8 @@
+package edu.teco.smartlambda.execution;
+
+/**
+ * A annotation marking a function as a lambda function
+ */
+public @interface LambdaFunction {
+	
+}
---
+++ execution/src/main/java/edu/teco/smartlambda/execution/LambdaParameter.java
@@ -0,0 +1,10 @@
+package edu.teco.smartlambda.execution;
+
+import java.io.Serializable;
+
+/**
+ * An intentionally empty interface marking a class as a serializable data class that a lambda function uses as a parameter
+ */
+public interface LambdaParameter extends Serializable {
+	
+}
