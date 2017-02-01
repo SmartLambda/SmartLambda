cc0cdd174fe724518525713cc7911073c025ce52
Melanie <uwedy@student.kit.edu>
2017-02-01 14:30:44 +0100

    Created package monitoring with all classes Created package lambda with all classes

---
+++ src/main/java/edu/teco/smartlambda/lambda/PrivilegedMonitoredLambdaFactory.java
@@ -0,0 +1,13 @@
+package edu.teco.smartlambda.lambda;
+
+/**
+ * Created by Melanie on 01.02.2017.
+ */
+public class PrivilegedMonitoredLambdaFactory extends LambdaFactory {
+	public AbstractLambda createLambda() {
+		return null;
+	}
+	public AbstractLambda getLambdaByName() {
+		return null;
+	}
+}
