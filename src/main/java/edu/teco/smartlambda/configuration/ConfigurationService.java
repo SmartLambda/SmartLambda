802cd8c881551ab24dc134f161bb9c99f0b1df19
Yussuf Khalil <uyebg@student.kit.edu>
2017-02-01 14:47:38 +0100

    Add ConfigurationService stub with basic tests

---
+++ src/main/java/edu/teco/smartlambda/configuration/ConfigurationService.java
@@ -0,0 +1,16 @@
+package edu.teco.smartlambda.configuration;
+
+import org.apache.commons.configuration2.Configuration;
+
+public class ConfigurationService {
+	private ConfigurationService() {
+	}
+	
+	public static ConfigurationService getInstance() {
+		return null;
+	}
+	
+	public Configuration getConfiguration() {
+		return null;
+	}
+}
