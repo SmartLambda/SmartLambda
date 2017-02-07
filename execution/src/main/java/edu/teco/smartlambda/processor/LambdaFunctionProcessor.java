965a53a2360628cb1ba48ff06469896c329d2891
Johannes Hengstler <ubezl@student.kit.edu>
2017-02-07 17:17:35 +0100

    Incomplete implementation of lambda function processor

--- execution/src/main/java/edu/teco/smartlambda/processor/LambdaFunctionProcessor.java
+++ execution/src/main/java/edu/teco/smartlambda/processor/LambdaFunctionProcessor.java
@@ -2,21 +2,30 @@ package edu.teco.smartlambda.processor;
 
 import com.google.auto.service.AutoService;
 import edu.teco.smartlambda.execution.LambdaFunction;
+import edu.teco.smartlambda.execution.LambdaParameter;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeMirror;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * An annotation processor that generates the required cconfiguration file for an archive containing a SmartLambda function
+ * An annotation processor that generates the required configuration file for an archive containing a SmartLambda function
  */
 @AutoService(Processor.class)
 public class LambdaFunctionProcessor extends AbstractProcessor {
+	
+	private TypeMirror lambdaParameterType;
 	
 	@Override
 	public Set<String> getSupportedAnnotationTypes() {
@@ -29,7 +38,44 @@ public class LambdaFunctionProcessor extends AbstractProcessor {
 	}
 	
 	@Override
+	public synchronized void init(ProcessingEnvironment processingEnv) {
+		super.init(processingEnv);
+		
+		// get the type mirror of LambdaParameters
+		lambdaParameterType = processingEnv.getElementUtils().getTypeElement(LambdaParameter.class.getCanonicalName()).asType();
+	}
+	
+	@Override
 	public boolean process(final Set<? extends TypeElement> set, final RoundEnvironment roundEnvironment) {
-		return false;
+		for (final Element element : roundEnvironment.getElementsAnnotatedWith(LambdaFunction.class)) {
+			assert element instanceof ExecutableElement; // forced through RetentionPolicy
+			
+			final ExecutableElement               functionElement = (ExecutableElement) element;
+			final List<? extends VariableElement> parameters      = functionElement.getParameters();
+			
+			String  lambdaFunctionEnclosingClassName;
+			String  lambdaFunctionName;
+			boolean hasParameter;
+			boolean hasReturnValue;
+			String  lambdaParameterClassName;
+			String  lambdaReturnValueClassName;
+			
+			// TODO get meta data
+			
+			if (parameters.size() > 1) {
+				throw new IllegalLambdaFunctionException("Illegal amount of lambda parameters. Maximum allowed: 1");
+			} else if (parameters.size() == 1) {
+				if (!processingEnv.getTypeUtils().isAssignable(parameters.get(0).asType(), lambdaParameterType)) {
+					throw new IllegalLambdaFunctionException("Lambda parameter does not implement " + LambdaParameter.class.getCanonicalName());
+				}
+				
+				hasParameter = true;
+				System.out.println(parameters.get(0).asType().toString());
+			}
+			
+			// TODO write meta file
+		}
+		
+		return true;
 	}
 }
