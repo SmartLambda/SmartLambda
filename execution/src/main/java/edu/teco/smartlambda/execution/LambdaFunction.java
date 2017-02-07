509274769d37e54c95d9eb895845b6baecc0e74f
Johannes Hengstler <ubezl@student.kit.edu>
2017-02-07 15:30:22 +0100

    Added retention, target and documented-tag to lambda-function

--- execution/src/main/java/edu/teco/smartlambda/execution/LambdaFunction.java
+++ execution/src/main/java/edu/teco/smartlambda/execution/LambdaFunction.java
@@ -1,8 +1,17 @@
 package edu.teco.smartlambda.execution;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * A annotation marking a function as a lambda function
  */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+@Documented
 public @interface LambdaFunction {
 	
 }
