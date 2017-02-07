aec1fa36690859bf32e84e291ddca5f70c87aaaa
Johannes Hengstler <ubezl@student.kit.edu>
2017-02-07 17:15:59 +0100

    Incomplete implementation of lambda execution service

--- execution/src/main/java/edu/teco/smartlambda/execution/LambdaExecutionService.java
+++ execution/src/main/java/edu/teco/smartlambda/execution/LambdaExecutionService.java
@@ -1,15 +1,44 @@
 package edu.teco.smartlambda.execution;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.Cleanup;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * TODO javadoc
  */
 public class LambdaExecutionService {
 	
+	public static final String LAMBDA_META_DATA_FILE = ".lambda";
+	
 	private static final int PORT = 31337;
 	
 	public static void main(final String... args) throws IOException {
+		@Cleanup final ServerSocket     socket       = new ServerSocket(PORT);
+		@Cleanup final Socket           clientSocket = socket.accept();
+		@Cleanup final DataInputStream  input        = new DataInputStream(clientSocket.getInputStream());
+		@Cleanup final DataOutputStream output       = new DataOutputStream(clientSocket.getOutputStream());
 		
+		final String jsonParameter = input.readUTF();
+		final Gson   gson          = new GsonBuilder().create();
+		
+		// TODO load lambda archive into classpath
+		
+		// TODO deserialize parameter
+		
+		// TODO start and execute lambda with parameter and gather return value
+		
+		// TODO serialize return value
+		final String returnValue = "";
+		
+		output.flush();
+		output.writeUTF(returnValue);
+		output.flush();
 	}
 }
