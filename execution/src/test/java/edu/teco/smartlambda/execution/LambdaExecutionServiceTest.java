fc404c03fa432c76976be6843cdde82751b690b5
Cydhra <ubezl@student.kit.edu>
2017-02-10 15:16:41 +0100

    Added unit test for LambdaExectionService

--- execution/src/test/java/edu/teco/smartlambda/execution/LambdaExecutionServiceTest.java
+++ execution/src/test/java/edu/teco/smartlambda/execution/LambdaExecutionServiceTest.java
@@ -1,8 +1,15 @@
 package edu.teco.smartlambda.execution;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  *
@@ -10,7 +17,27 @@ import static org.junit.Assert.*;
 public class LambdaExecutionServiceTest {
 	
 	@Test
-	public void testSocket() {
+	public void testLambdaExection() throws IOException {
+		// the LambdaExectionService expects the lambda.jar existing
+		assert new File("lambda.jar").exists() : "the test lambda archive is missing";
 		
+		// start the exection service asynchronously
+		new Thread(LambdaExecutionService::main).start();
+		
+		final Gson             gson         = new GsonBuilder().create();
+		final Socket           socket       = new Socket("localhost", 31337);
+		final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+		
+		outputStream.writeUTF("{\"demoValue\": \"string\"}");
+		outputStream.flush();
+		
+		final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+		
+		final ExecutionReturnValue returnValue = gson.fromJson(inputStream.readUTF(), ExecutionReturnValue.class);
+		
+		Assert.assertNull(returnValue.getException());
+		Assert.assertNotNull(returnValue.getReturnValue());
+		
+		Assert.assertEquals("{\"demoReturnValue\": \"success\"]", returnValue.getReturnValue());
 	}
 }
