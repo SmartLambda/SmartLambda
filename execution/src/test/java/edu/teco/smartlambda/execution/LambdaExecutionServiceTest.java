109cbae1f0cf669d7522ba38af6a683f022cbd6c
Cydhra <ubezl@student.kit.edu>
2017-02-10 14:19:25 +0100

    Added unit test for LambdaExectionService

---
+++ execution/src/test/java/edu/teco/smartlambda/execution/LambdaExecutionServiceTest.java
@@ -0,0 +1,16 @@
+package edu.teco.smartlambda.execution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class LambdaExecutionServiceTest {
+	
+	@Test
+	public void testSocket() {
+		
+	}
+}
