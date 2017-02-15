033f85c8c85bc655903fd44b83215011415b62cc
Yussuf Khalil <uyebg@student.kit.edu>
2017-02-15 15:25:37 +0100

    Implement Hibernate initialization and initialize Spark with controller and filter stubs

---
+++ src/main/java/edu/teco/smartlambda/rest/controller/LambdaController.java
@@ -0,0 +1,34 @@
+package edu.teco.smartlambda.rest.controller;
+
+import spark.Request;
+import spark.Response;
+
+public class LambdaController {
+	public static Object createLambda(final Request request, final Response response) {
+		return null;
+	}
+	
+	public static Object updateLambda(final Request request, final Response response) {
+		return null;
+	}
+	
+	public static Object readLambda(final Request request, final Response response) {
+		return null;
+	}
+	
+	public static Object deleteLambda(final Request request, final Response response) {
+		return null;
+	}
+	
+	public static Object executeLambda(final Request request, final Response response) {
+		return null;
+	}
+	
+	public static Object getLambdaList(final Request request, final Response response) {
+		return null;
+	}
+	
+	public static Object getStatistics(final Request request, final Response response) {
+		return null;
+	}
+}
---
+++ src/main/java/edu/teco/smartlambda/rest/filter/SessionEndFilter.java
@@ -0,0 +1,11 @@
+package edu.teco.smartlambda.rest.filter;
+
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+
+public class SessionEndFilter implements Filter {
+	@Override
+	public void handle(final Request request, final Response response) throws Exception {
+	}
+}
