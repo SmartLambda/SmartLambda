2c6854b2d6a6f0576baadfab7234b7756906dc44
Cydhra <ubezl@student.kit.edu>
2017-02-15 15:01:11 +0100

    Bugfix: Threadlocal not correctly implemented

--- src/main/java/edu/teco/smartlambda/authentication/AuthenticationService.java
+++ src/main/java/edu/teco/smartlambda/authentication/AuthenticationService.java
@@ -1,49 +1,53 @@
 package edu.teco.smartlambda.authentication;
-import edu.teco.smartlambda.authentication.entities.*;
+
+import edu.teco.smartlambda.authentication.entities.Key;
+import edu.teco.smartlambda.authentication.entities.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
 import java.util.Optional;
 
 /**
  * Created by Jonathan on 01.02.17.
  */
 public class AuthenticationService {
-	private static Configuration conf = new Configuration();
-	public static SessionFactory sessionFactory = conf.buildSessionFactory();
-	private static ThreadLocal<AuthenticationService> ourInstance = null;
-	private Key authenticatedKey = null;
+	
+	private static Configuration                      conf             = new Configuration();
+	public static  SessionFactory                     sessionFactory   = conf.buildSessionFactory();
+	private static ThreadLocal<AuthenticationService> instance         = null;
+	private        Key                                authenticatedKey = null;
 	
 	public static AuthenticationService getInstance() {
-		if (ourInstance == null) {
-			AuthenticationService as = new AuthenticationService();
-			ourInstance = new ThreadLocal<AuthenticationService>();
-			ourInstance.set(as);
+		if (instance == null) {
+			instance = new ThreadLocal<>();
+			instance.set(new AuthenticationService());
+		} else if (instance.get() == null) {
+			instance.set(new AuthenticationService());
 		}
-		return ourInstance.get();
+		
+		return instance.get();
 	}
 	
 	private AuthenticationService() {
 		
 	}
 	
-	public void authenticate(String key) {
+	public void authenticate(final String key) {
 		//TODO hash parameter key and search for it in the database. Then assign it to the local variable "authenticatedKey"
 	}
 	
-	public void authenticate(Key key) {
+	public void authenticate(final Key key) {
 		authenticatedKey = key;
 	}
-
+	
 	public Optional<Key> getAuthenticatedKey() {
-		return new Optional<Key>.ofNullable(authenticatedKey);
+		return new Optional.ofNullable(authenticatedKey);
 	}
 	
 	public Optional<User> getAuthenticatedUser() {
 		if (authenticatedKey != null) {
-			return new Optional<Key>.ofNullable(authenticatedKey.getUser());
+			return new Optional.of(authenticatedKey.getUser());
 		}
 		return Optional.empty();
 	}
-	
-	
 }
