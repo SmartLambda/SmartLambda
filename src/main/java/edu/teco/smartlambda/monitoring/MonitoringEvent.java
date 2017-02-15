608beaafbdb89939e9070a4aa46d6308b7d1057f
Cydhra <ubezl@student.kit.edu>
2017-02-15 15:11:56 +0100

    Refactored MonitoringEvent

--- src/main/java/edu/teco/smartlambda/monitoring/MonitoringEvent.java
+++ src/main/java/edu/teco/smartlambda/monitoring/MonitoringEvent.java
@@ -1,36 +1,21 @@
 package edu.teco.smartlambda.monitoring;
 
 import edu.teco.smartlambda.authentication.entities.User;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.Calendar;
 
-/**
- * Created by Melanie on 29.01.2017.
- */
+@Data
 public class MonitoringEvent {
-	@Getter
-	@Setter
-	private Calendar            time;
-	@Getter
-	@Setter
-	private User                lambdaOwner;
-	@Getter
-	@Setter
-	private String              lambdaName;
-	@Getter
-	@Setter
-	private long                duration;
-	@Getter
-	@Setter
-	private int                 CPUTime;
-	@Getter
-	@Setter
-	private String              error;
-	@Getter
-	@Setter
-	private MonitoringEventType status;
+	
+	// TODO are there more immutable attributes?
+	private       Calendar            time;
+	private final User                lambdaOwner;
+	private final String              lambdaName;
+	private       long                duration;
+	private       int                 CPUTime;
+	private       String              error;
+	private final MonitoringEventType status;
 	
 	enum MonitoringEventType {
 		EXECUTION, DELETION, DEPLOYMENT;
