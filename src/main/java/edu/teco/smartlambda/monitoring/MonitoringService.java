ebc1a120022db7549e9e2e4c2abfd3098f5bf22a
Melanie <uwedy@student.kit.edu>
2017-02-17 17:33:12 +0100

    Persistence for MonitoringEvent added

--- src/main/java/edu/teco/smartlambda/monitoring/MonitoringService.java
+++ src/main/java/edu/teco/smartlambda/monitoring/MonitoringService.java
@@ -1,24 +1,30 @@
 package edu.teco.smartlambda.monitoring;
 
+import edu.teco.smartlambda.authentication.AuthenticationService;
+import edu.teco.smartlambda.authentication.NotAuthenticatedException;
 import edu.teco.smartlambda.lambda.AbstractLambda;
 import lombok.Getter;
 
+
 import java.util.Calendar;
+import java.util.function.Supplier;
 
 /**
  * Created by Melanie on 29.01.2017.
  */
-//// FIXME: 2/15/17 TODO persist the monitoring events
+	
 public class MonitoringService {
 	
 	private ThreadLocal<MonitoringService> instance;
 	@Getter
 	private MonitoringEvent                monitoringEvent;
+	private AuthenticationService authenticationService = AuthenticationService.getInstance();
 	
 	public MonitoringService() {}
 	
 	public void onLambdaExecutionStart(final AbstractLambda lambda) {
-		monitoringEvent = new MonitoringEvent(lambda.getOwner(), lambda.getName(), MonitoringEvent.MonitoringEventType.EXECUTION);
+		monitoringEvent = new MonitoringEvent(lambda.getOwner(), lambda.getName(), MonitoringEvent.MonitoringEventType.EXECUTION,
+				authenticationService.getAuthenticatedKey().orElseThrow(NotAuthenticatedException::new));
 		monitoringEvent.setTime(Calendar.getInstance());
 	}
 	
@@ -34,12 +40,14 @@ public class MonitoringService {
 	}
 	
 	public void onLambdaDeletion(final AbstractLambda lambda) {
-		monitoringEvent = new MonitoringEvent(lambda.getOwner(), lambda.getName(), MonitoringEvent.MonitoringEventType.DELETION);
+		monitoringEvent = new MonitoringEvent(lambda.getOwner(), lambda.getName(), MonitoringEvent.MonitoringEventType.DELETION,
+				authenticationService.getAuthenticatedKey().orElseThrow(NotAuthenticatedException::new));
 		monitoringEvent.setTime(Calendar.getInstance());
 	}
 	
 	public void onLambdaDeployment(final AbstractLambda lambda) {
-		monitoringEvent = new MonitoringEvent(lambda.getOwner(), lambda.getName(), MonitoringEvent.MonitoringEventType.DEPLOYMENT);
+		monitoringEvent = new MonitoringEvent(lambda.getOwner(), lambda.getName(), MonitoringEvent.MonitoringEventType.DEPLOYMENT,
+				authenticationService.getAuthenticatedKey().orElseThrow(NotAuthenticatedException::new));
 		monitoringEvent.setTime(Calendar.getInstance());
 	}
 }
