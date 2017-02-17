2bc34fde429e858691e5d3e0169df1f291355f6a
Jonathan Böttcher <utefh@student.kit.edu>
2017-02-17 16:08:06 +0100

    NotAuthenticatedException as RuntimeException

--- src/main/java/edu/teco/smartlambda/authentication/NotAuthenticatedException.java
+++ src/main/java/edu/teco/smartlambda/authentication/NotAuthenticatedException.java
@@ -3,6 +3,6 @@ package edu.teco.smartlambda.authentication;
 /**
  * Created on 17.02.17.
  */
-public class NotAuthenticatedException extends Exception {
+public class NotAuthenticatedException extends RuntimeException {
 	
 }
