355315cce3bd2f4697799795750a269fec2ba161
Johannes Hengstler <ubezl@student.kit.edu>
2017-02-20 17:32:40 +0100

    Implemented ContainerBuilder interface

---
+++ src/main/java/edu/teco/smartlambda/container/ContainerBuilder.java
@@ -0,0 +1,46 @@
+package edu.teco.smartlambda.container;
+
+/**
+ * A builder for container files that shall be configured to execute the lambda on spawn
+ */
+public interface ContainerBuilder {
+	
+	/**
+	 * Builds a new {@link Container} instance with the previously configured settings
+	 *
+	 * @return a new concrete Container instance
+	 */
+	public Container build();
+	
+	/**
+	 * Append a command to a container file that is executed upon container spawn
+	 *
+	 * @param command command to be executed
+	 *
+	 * @return this builder instance
+	 */
+	public ContainerBuilder appendCommand(final String command);
+	
+	/**
+	 * Stores a non-executable file in the container
+	 *
+	 * @param binary binary content
+	 * @param name   file name
+	 *
+	 * @return this builder instance
+	 */
+	public default ContainerBuilder storeFile(final byte[] binary, final String name) {
+		return this.storeFile(binary, name, false);
+	}
+	
+	/**
+	 * Stores a file in the container and sets the executable flag if required
+	 *
+	 * @param binary     binary content
+	 * @param name       file name
+	 * @param executable whether the file is executable
+	 *
+	 * @return this builder instance
+	 */
+	public ContainerBuilder storeFile(final byte[] binary, final String name, final boolean executable);
+}
