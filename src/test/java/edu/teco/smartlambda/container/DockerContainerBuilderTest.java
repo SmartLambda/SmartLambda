ccb487f946271493671d72a553db32480d41fd07
Johannes Hengstler <ubezl@student.kit.edu>
2017-02-22 16:12:28 +0100

    Test case now deletes container

--- src/test/java/edu/teco/smartlambda/container/DockerContainerBuilderTest.java
+++ src/test/java/edu/teco/smartlambda/container/DockerContainerBuilderTest.java
@@ -10,5 +10,6 @@ public class DockerContainerBuilderTest {
 	@Test
 	public void simpleBuildTest() throws Exception {
 		Container container = new DockerContainerBuilder().setCommand("example command").build();
+		container.delete();
 	}
 }
