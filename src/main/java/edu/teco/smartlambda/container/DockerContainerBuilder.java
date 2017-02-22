a9a1da000ddbe12ef537f062d3b53fe524fc5a24
Johannes Hengstler <ubezl@student.kit.edu>
2017-02-22 14:36:04 +0100

    Finished implementation of DockerContainerBuilder

--- src/main/java/edu/teco/smartlambda/container/DockerContainerBuilder.java
+++ src/main/java/edu/teco/smartlambda/container/DockerContainerBuilder.java
@@ -5,10 +5,11 @@ import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.exceptions.DockerCertificateException;
 import com.spotify.docker.client.exceptions.DockerException;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -16,40 +17,43 @@ import java.util.UUID;
  */
 public class DockerContainerBuilder implements ContainerBuilder {
 	
-	private String command;
+	private final String containerId;
+	private       String command;
+	private final File   tmpDirectory;
+	
+	public DockerContainerBuilder() {
+		containerId = generateContainerId();
+		
+		tmpDirectory = new File(System.getProperty("java.io.tmpdir"), containerId);
+		assert !tmpDirectory.exists() : "Temporary docker file directory already exists!";
+		
+		//noinspection ResultOfMethodCallIgnored
+		tmpDirectory.mkdir();
+	}
 	
 	@Override
 	public DockerContainer build() throws DockerCertificateException, IOException, DockerException, InterruptedException {
-		final String containerId = generateContainerId();
-		
-		final File tmpDirectory = new File(containerId);
-		assert !tmpDirectory.exists() : "Temporary docker file directory already exists!";
-		//noinspection ResultOfMethodCallIgnored
-		tmpDirectory.mkdir();
-		
 		final File       dockerFile = new File(tmpDirectory, "Dockerfile");
 		final FileWriter writer     = new FileWriter(dockerFile);
 		
 		//// FIXME: 2/20/17
 		writer.write("FROM openjdk:8\n");
-		writer.write("COPY . /usr/workspace\n");
-		writer.write("WORKDIR /usr/workspace\n");
+		writer.write("COPY . ~\n");
+		writer.write("WORKDIR ~\n");
 		writer.write("EXPOSE 31337\n");
 		writer.write("CMD " + this.command + "\n");
 		writer.flush();
 		writer.close();
 		
-		final DockerClient dockerClient = DefaultDockerClient.fromEnv().build();
+		//// FIXME: 2/22/17 
+		final DockerClient dockerClient = new DefaultDockerClient("unix:///var/run/docker.sock");
 		System.out.println(tmpDirectory.getAbsoluteFile().toPath());
 		final String imageId = dockerClient.build(tmpDirectory.getAbsoluteFile().toPath(), DockerClient.BuildParam.name(containerId));
 		
 		//noinspection ResultOfMethodCallIgnored
 		tmpDirectory.delete();
 		
-		// TODO debug code
-		if (!Objects.equals(imageId, containerId)) throw new AssertionError("image id != container id");
-		
-		return new DockerContainer(containerId);
+		return new DockerContainer(imageId);
 	}
 	
 	@Override
@@ -59,8 +63,22 @@ public class DockerContainerBuilder implements ContainerBuilder {
 	}
 	
 	@Override
-	public ContainerBuilder storeFile(final byte[] binary, final String name, final boolean executable) {
-		//// FIXME: 2/20/17
+	public ContainerBuilder storeFile(final byte[] binary, final String name, final boolean executable) throws IOException {
+		final File file = new File(tmpDirectory, name);
+		assert !file.exists();
+		
+		//noinspection ResultOfMethodCallIgnored
+		file.createNewFile();
+		
+		final DataOutputStream output = new DataOutputStream(new FileOutputStream(file));
+		output.write(binary);
+		output.flush();
+		output.close();
+		
+		//noinspection ResultOfMethodCallIgnored
+		file.setExecutable(executable);
+		assert file.canExecute();
+		
 		return this;
 	}
 	
