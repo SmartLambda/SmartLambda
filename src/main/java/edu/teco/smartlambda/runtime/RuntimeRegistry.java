35e5500ced4935328958bf7b9a18159c6ad65c4b
Yussuf Khalil <uyebg@student.kit.edu>
2017-02-22 13:50:42 +0100

    Implement runtime registry initialization

--- src/main/java/edu/teco/smartlambda/runtime/RuntimeRegistry.java
+++ src/main/java/edu/teco/smartlambda/runtime/RuntimeRegistry.java
@@ -1,6 +1,11 @@
 package edu.teco.smartlambda.runtime;
 
+import edu.teco.smartlambda.configuration.ConfigurationService;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,11 +18,33 @@ public class RuntimeRegistry {
 	private final Map<String, Runtime> runtimes = new HashMap<>();
 	
 	private RuntimeRegistry() {
-		// TODO load registry instances from config
+		final List<String> runtimes = ConfigurationService.getInstance().getConfiguration().getList(String.class, "runtimes.runtime");
+		
+		if (runtimes == null) {
+			LoggerFactory.getLogger(RuntimeRegistry.class).error("No runtimes defined");
+			return;
+		}
+		
+		for (String runtime : runtimes) {
+			try {
+				final Runtime runtimeInstance = (Runtime) Class.forName(runtime).getConstructor().newInstance();
+				this.runtimes.put(runtime, runtimeInstance);
+				LoggerFactory.getLogger(RuntimeRegistry.class).info("Loaded runtime " + runtime);
+			} catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+				LoggerFactory.getLogger(RuntimeRegistry.class).error("Exception while loading runtime");
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				LoggerFactory.getLogger(RuntimeRegistry.class).error("Runtime " + runtime + " not found");
+			}
+		}
 	}
 	
 	public static RuntimeRegistry getInstance() {
 		if (instance == null) instance = new RuntimeRegistry();
 		return instance;
+	}
+	
+	public Runtime getRuntimeByName(final String name) {
+		return runtimes.get(name);
 	}
 }
