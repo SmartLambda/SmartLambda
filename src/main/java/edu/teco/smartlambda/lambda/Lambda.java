dce7a4b7abba9aa1a8a5091127287ff75847cc94
Melanie <uwedy@student.kit.edu>
2017-02-26 15:17:56 +0100

    Implemented Execute, Save in Lambda Added Persistence in Monitoring

--- src/main/java/edu/teco/smartlambda/lambda/Lambda.java
+++ src/main/java/edu/teco/smartlambda/lambda/Lambda.java
@@ -1,43 +1,108 @@
 package edu.teco.smartlambda.lambda;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.teco.smartlambda.Application;
 import edu.teco.smartlambda.authentication.entities.User;
+import edu.teco.smartlambda.container.BuilderFactory;
+import edu.teco.smartlambda.container.Container;
+import edu.teco.smartlambda.container.ContainerBuilder;
+import edu.teco.smartlambda.container.ContainerFactory;
 import edu.teco.smartlambda.monitoring.MonitoringEvent;
 import edu.teco.smartlambda.runtime.Runtime;
 import edu.teco.smartlambda.runtime.RuntimeRegistry;
 import edu.teco.smartlambda.schedule.Event;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Session;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Table;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.List;
 import java.util.Optional;
+import static org.torpedoquery.jpa.Torpedo.from;
+import static org.torpedoquery.jpa.Torpedo.select;
+import static org.torpedoquery.jpa.Torpedo.where;
 
+@Entity
+@Table(appliesTo = "Lambda")
 public class Lambda extends AbstractLambda {
 	
 	public static final int PORT = 4_0_0_0_1;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
 	@Getter
 	@Setter
+	@Column(name = "owner")
 	private User owner;
 	
 	@Getter
 	@Setter
+	@Column(name = "name")
 	private String name;
 	
 	@Getter
 	@Setter
+	@Column(name = "async")
 	private boolean async;
 	
+	@Column(name = "runtime")
 	private String runtime;
+	
+	private String containerId;
+	
+	@Transient
+	private ContainerBuilder builder = BuilderFactory.getContainerBuilder();
+	
+	private Session session = Application.getInstance().getSessionFactory().openSession();
 	
 	@Override
 	public Optional<ExecutionReturnValue> execute(final String params, final boolean async) {
-		//// FIXME: 2/15/17 
-		return null;
+		Container container = ContainerFactory.getContainerById(containerId);
+		try {
+			container.start();
+		} catch (Exception e) {
+			throw (new RuntimeException(e));
+		}
+		final Gson   gson = new GsonBuilder().create();
+		final Socket socket;
+		final DataInputStream inputStream;
+		final ExecutionReturnValue returnValue;
+		try {
+			socket = new Socket("localhost", PORT);
+			inputStream = new DataInputStream(socket.getInputStream());
+			returnValue = gson.fromJson(inputStream.readUTF(), ExecutionReturnValue.class);
+		} catch (Exception e) {
+			throw (new RuntimeException(e));
+		}
+		
+		return Optional.of(returnValue);
 	}
 	
 	@Override
 	public void save() {
-		//// FIXME: 2/15/17 
+		try {
+			Container container = builder.build();
+			containerId = container.getContainerId();
+		} catch (Exception e) {
+			throw (new RuntimeException(e));
+		}
+		session.beginTransaction();
+		session.save(this);
+		session.getTransaction().commit();
 	}
 	
 	@Override
@@ -47,7 +112,8 @@ public class Lambda extends AbstractLambda {
 	
 	@Override
 	public void delete() {
-		//// FIXME: 2/15/17 
+		Object lambda = session.load(Lambda.class, id);
+		session.delete(lambda);
 	}
 	
 	@Override
@@ -74,8 +140,10 @@ public class Lambda extends AbstractLambda {
 	
 	@Override
 	public List<MonitoringEvent> getMonitoringEvents() {
-		//// FIXME: 2/15/17 
-		return null;
+		MonitoringEvent query = from(MonitoringEvent.class);
+		where(query.getLambdaName()).eq(name).and(query.getLambdaOwner()).eq(owner);
+		
+		return select(query).list(session);
 	}
 	
 	@Override
--- src/main/java/edu/teco/smartlambda/lambda/MonitoringDecorator.java
+++ src/main/java/edu/teco/smartlambda/lambda/MonitoringDecorator.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 /**
  * Decorates lambdas with calls to the monitoring service
  */
-//// FIXME: 2/15/17 
+
 public class MonitoringDecorator extends LambdaDecorator {
 	
 	public MonitoringDecorator(final AbstractLambda lambda) {
