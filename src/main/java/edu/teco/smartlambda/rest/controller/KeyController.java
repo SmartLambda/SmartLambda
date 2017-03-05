a6cc4f977f5db61f18316267f394f1e339d4b343
Yussuf Khalil <uyebg@student.kit.edu>
2017-03-05 16:37:55 +0100

    Implement PUT /key REST method

--- src/main/java/edu/teco/smartlambda/rest/controller/KeyController.java
+++ src/main/java/edu/teco/smartlambda/rest/controller/KeyController.java
@@ -1,11 +1,18 @@
 package edu.teco.smartlambda.rest.controller;
 
+import edu.teco.smartlambda.authentication.AuthenticationService;
+import edu.teco.smartlambda.authentication.InsufficientPermissionsException;
+import edu.teco.smartlambda.authentication.NameConflictException;
+import edu.teco.smartlambda.authentication.NotAuthenticatedException;
+import edu.teco.smartlambda.authentication.entities.User;
 import spark.Request;
 import spark.Response;
 
 public class KeyController {
-	public static Object createKey(final Request request, final Response response) {
-		return null;
+	public static Object createKey(final Request request, final Response response)
+			throws InsufficientPermissionsException, NameConflictException {
+		final User user = AuthenticationService.getInstance().getAuthenticatedUser().orElseThrow(NotAuthenticatedException::new);
+		return user.createKey(request.params(":name")).getRight();
 	}
 	
 	public static Object deleteKey(final Request request, final Response response) {
