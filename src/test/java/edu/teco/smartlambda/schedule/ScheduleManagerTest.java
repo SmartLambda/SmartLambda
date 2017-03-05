8ab9a7814991dd968b2c4b8457446c0b862deecf
Melanie <uwedy@student.kit.edu>
2017-03-05 19:40:22 +0100

    Changed build path for Windows

--- src/test/java/edu/teco/smartlambda/schedule/ScheduleManagerTest.java
+++ src/test/java/edu/teco/smartlambda/schedule/ScheduleManagerTest.java
@@ -7,9 +7,12 @@ import edu.teco.smartlambda.monitoring.MonitoringEvent;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.mockito.Mockito;
 import org.torpedoquery.jpa.Torpedo;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.torpedoquery.jpa.Torpedo.from;
@@ -21,7 +24,8 @@ import static org.torpedoquery.jpa.Torpedo.select;
 public class ScheduleManagerTest {
 	
 	ListenableFuture<Void> future;
-	List<Event>            events;
+	List<Event> events = new LinkedList<>();
+	Event someEvent;
 	
 	@Before
 	public void setUp() {
@@ -32,16 +36,25 @@ public class ScheduleManagerTest {
 		for (Event e : events) {
 			e.save();
 		}
+		someEvent = Mockito.mock(Event.class);
 	}
 	
+	@Test
+	@Ignore
 	public void runTest() {
-		ScheduleManager.getInstance().run();
 		MonitoringEvent query = from(MonitoringEvent.class);
-		Torpedo.where(query.getLambdaName()).eq(events.get(0).getLambda().getName()).and(query.getLambdaOwner())
-				.eq(events.get(0).getLambda().getOwner());
-		MonitoringEvent result =
-				select(query).setMaxResults(1).get(Application.getInstance().getSessionFactory().getCurrentSession()).orElse(null);
+		Torpedo.where(query.getLambdaName()).eq(events.get(1).getLambda().getName()).and(query.getLambdaOwner())
+				.eq(events.get(1).getLambda().getOwner());
+		MonitoringEvent result = select(query).setMaxResults(1).get(Application.getInstance().getSessionFactory().getCurrentSession()).orElse(null);
 		Assert.assertNotNull(result);
+	}
+	
+	@Test
+	public void saveTest() {
+		Event query = from(Event.class);
+		Torpedo.where(query.getName()).eq(someEvent.getName());
+		Event result = select(query).setMaxResults(1).get(Application.getInstance().getSessionFactory().getCurrentSession()).orElse(null);
+		Assert.assertEquals(someEvent, result);
 	}
 	
 	@After
