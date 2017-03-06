51103353a01d1e8df7c32b79ab9b16179eb1e324
Melanie <uwedy@student.kit.edu>
2017-03-06 18:16:06 +0100

    Changed List for futures to HashMap in run in ScheduleManager

--- src/main/java/edu/teco/smartlambda/schedule/ScheduleManager.java
+++ src/main/java/edu/teco/smartlambda/schedule/ScheduleManager.java
@@ -8,10 +8,8 @@ import lombok.Setter;
 import org.hibernate.Session;
 
 import javax.persistence.LockModeType;
-import java.util.AbstractMap;
 import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
 
 import static org.torpedoquery.jpa.Torpedo.from;
 import static org.torpedoquery.jpa.Torpedo.select;
@@ -33,30 +31,31 @@ public class ScheduleManager {
 		return instance;
 	}
 	
-	private void ScheduleManager() {}
+	private ScheduleManager() {}
 	
 	public Void run() {
-		Event                                                                        event;
-		List<AbstractMap.SimpleEntry<Event, ListenableFuture<ExecutionReturnValue>>> futures = new LinkedList<>();
+		
+		final HashMap<Event, ListenableFuture<ExecutionReturnValue>> futures = new HashMap<>(0);
 		
 		while (notEnd) {
-			Session session = Application.getInstance().getSessionFactory().getCurrentSession();
+			final Event   event;
+			final Session session = Application.getInstance().getSessionFactory().getCurrentSession();
 			session.beginTransaction();
 			
 			//Checks all started futures, if they're still running -> update lock, else -> update nextExecutionTime
-			for (AbstractMap.SimpleEntry<Event, ListenableFuture<ExecutionReturnValue>> future : futures) {
-				if (!future.getValue().isDone()) {
-					future.getKey().setLock(Calendar.getInstance());
-					session.update(future.getKey());
+			futures.forEach((Event e, ListenableFuture<ExecutionReturnValue> future) -> {
+				if (!future.isDone()) {
+					e.setLock(Calendar.getInstance());
+					session.update(e);
 				} else {
-					future.getKey().save();
-					futures.remove(future);
+					e.save();
+					futures.remove(e, future);
 				}
-			}
+			});
 			
-			Event    query         = from(Event.class);
-			Calendar lockTolerance = Calendar.getInstance();
-			lockTolerance.add(Calendar.MINUTE, -2);
+			final Event    query         = from(Event.class);
+			final Calendar lockTolerance = Calendar.getInstance();
+			lockTolerance.add(Calendar.MINUTE, -5);
 			
 			//Gets next scheduled event from the database
 			where(query.getNextExecution()).lte(Calendar.getInstance()).and(query.getLock()).isNull().or(query.getLock())
@@ -67,7 +66,7 @@ public class ScheduleManager {
 				event.setLock(Calendar.getInstance());
 				session.update(event);
 				session.getTransaction().commit();
-				futures.add(new AbstractMap.SimpleEntry<>(event, event.execute()));
+				futures.put(event, event.execute());
 			}
 			
 			try {
