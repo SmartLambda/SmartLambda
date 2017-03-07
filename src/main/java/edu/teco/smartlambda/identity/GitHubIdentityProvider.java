7c06677e71333a7229356de8232c1cf9fb1790e4
Jonathan Böttcher <utefh@student.kit.edu>
2017-03-07 22:23:48 +0100

    Implemented GitHubIdentity classes still with missing github authentication call

---
+++ src/main/java/edu/teco/smartlambda/identity/GitHubIdentityProvider.java
@@ -0,0 +1,32 @@
+package edu.teco.smartlambda.identity;
+
+import edu.teco.smartlambda.Application;
+import edu.teco.smartlambda.authentication.entities.User;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Map;
+
+/**
+ * Created on 28.02.17.
+ */
+public class GitHubIdentityProvider implements IdentityProvider{
+	private static final String NAME = "github";
+	
+	@Override
+	public Pair<User, String> register(final Map<String, String> parameters) throws IdentityException {
+		final String accessToken = parameters.get("accessToken");
+		if (accessToken == null) {
+			throw new IdentitySyntaxException();
+		}
+		final GitHubCredential credential = new GitHubCredential(accessToken);
+		Application.getInstance().getSessionFactory().getCurrentSession().save(credential);
+		
+		
+		return User.createUser(""/*TODO*/);
+	}
+	
+	@Override
+	public String getName() {
+		return NAME;
+	}
+}
