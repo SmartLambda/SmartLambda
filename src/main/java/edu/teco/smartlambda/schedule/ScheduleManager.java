5cadebc7778af48302e75114f889bdd53a470626
Johannes Hengstler <ubezl@student.kit.edu>
2017-03-08 00:48:38 +0100

    Fixed: ScheduleManager would incorrectly rollback transactions with updated locks ScheduleManager would crash on removing executed futures from the map (concurrent modification)

--- src/main/java/edu/teco/smartlambda/schedule/ScheduleManager.java
+++ src/main/java/edu/teco/smartlambda/schedule/ScheduleManager.java
@@ -5,7 +5,6 @@ import edu.teco.smartlambda.Application;
 import edu.teco.smartlambda.runtime.ExecutionResult;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.Session;
 
 import javax.persistence.LockModeType;
 import java.util.Calendar;
@@ -39,42 +38,53 @@ public class ScheduleManager {
 	 * @return nothing
 	 */
 	public Void run() {
-		
 		final HashMap<Event, ListenableFuture<ExecutionResult>> futures = new HashMap<>(0);
 		
 		while (this.running) {
-			final Event   event;
-			final Session session = Application.getInstance().getSessionFactory().getCurrentSession();
-			session.beginTransaction();
-			
-			//Checks all started futures, if they're still running -> update lock, else -> update nextExecutionTime
-			futures.forEach((Event e, ListenableFuture<ExecutionResult> future) -> {
-				if (!future.isDone()) {
-					e.setLock(Calendar.getInstance());
-					session.update(e);
+			final Event event;
+			try {
+				
+				Application.getInstance().getSessionFactory().getCurrentSession().beginTransaction();
+				//Checks all started futures, if they're still running -> update lock, else -> update nextExecutionTime
+				futures.forEach((Event e, ListenableFuture<ExecutionResult> future) -> {
+					if (!future.isDone()) {
+						e.setLock(Calendar.getInstance());
+						Application.getInstance().getSessionFactory().getCurrentSession().update(e);
+						Application.getInstance().getSessionFactory().getCurrentSession().getTransaction().commit();
+					} else {
+						e.setLock(null);
+						e.save();
+						Application.getInstance().getSessionFactory().getCurrentSession().getTransaction().commit();
+					}
+				});
+				
+				futures.keySet().removeIf(v -> futures.get(v).isDone());
+				
+				if (Application.getInstance().getSessionFactory().getCurrentSession().getTransaction() == null ||
+						!Application.getInstance().getSessionFactory().getCurrentSession().getTransaction().isActive())
+					Application.getInstance().getSessionFactory().getCurrentSession().beginTransaction();
+				
+				final Event    query         = from(Event.class);
+				final Calendar lockTolerance = Calendar.getInstance();
+				lockTolerance.add(Calendar.MINUTE, -5);
+				
+				//Gets next scheduled event from the database
+				where(query.getNextExecution()).lte(Calendar.getInstance()).and(query.getLock()).isNull().or(query.getLock())
+						.lte(lockTolerance);
+				event = select(query).setLockMode(LockModeType.PESSIMISTIC_WRITE).setMaxResults(1)
+						.get(Application.getInstance().getSessionFactory().getCurrentSession()).orElse(null);
+				
+				if (event != null) {
+					event.setLock(Calendar.getInstance());
+					Application.getInstance().getSessionFactory().getCurrentSession().update(event);
+					futures.put(event, event.execute());
+					Application.getInstance().getSessionFactory().getCurrentSession().getTransaction().commit();
 				} else {
-					e.save();
-					futures.remove(e, future);
+					Application.getInstance().getSessionFactory().getCurrentSession().getTransaction().rollback();
 				}
-			});
-			
-			final Event    query         = from(Event.class);
-			final Calendar lockTolerance = Calendar.getInstance();
-			lockTolerance.add(Calendar.MINUTE, -5);
-			
-			//Gets next scheduled event from the database
-			where(query.getNextExecution()).lte(Calendar.getInstance()).and(query.getLock()).isNull().or(query.getLock()).lte(lockTolerance);
-			event = select(query).setLockMode(LockModeType.PESSIMISTIC_WRITE).setMaxResults(1).get(session).orElse(null);
-			
-			if (event != null) {
-				event.setLock(Calendar.getInstance());
-				session.update(event);
-				futures.put(event, event.execute());
-				session.getTransaction().commit();
-			} else {
-				session.getTransaction().rollback();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			
 			try {
 				Thread.sleep(1000);
 			} catch (final InterruptedException e) {
