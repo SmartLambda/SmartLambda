2daa0bc4a3e6438b9b33897ad74ec4467038a3ba
Yussuf Khalil <uyebg@student.kit.edu>
2017-03-07 20:03:21 +0100

    Map InsufficientPermissionsException to HTTP 403 response code

--- src/main/java/edu/teco/smartlambda/Application.java
+++ src/main/java/edu/teco/smartlambda/Application.java
@@ -1,6 +1,7 @@
 package edu.teco.smartlambda;
 
 import com.google.gson.Gson;
+import edu.teco.smartlambda.authentication.InsufficientPermissionsException;
 import edu.teco.smartlambda.authentication.NotAuthenticatedException;
 import edu.teco.smartlambda.authentication.entities.Key;
 import edu.teco.smartlambda.authentication.entities.Permission;
@@ -114,6 +115,11 @@ public class Application {
 		
 		Spark.exception(DuplicateLambdaException.class, (Exception exception, Request request, Response response) -> {
 			response.status(409);
+			response.body(gson.toJson(new ExceptionResponse(exception.getMessage())));
+		});
+		
+		Spark.exception(InsufficientPermissionsException.class, (Exception exception, Request request, Response response) -> {
+			response.status(403);
 			response.body(gson.toJson(new ExceptionResponse(exception.getMessage())));
 		});
 	}
