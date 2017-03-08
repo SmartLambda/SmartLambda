909081b65dd491ce2dc3f1a2b0a03387386fa4f0
Jonathan Böttcher <utefh@student.kit.edu>
2017-03-08 03:48:54 +0100

    Ignore Testcases with real github account

--- src/test/java/edu/teco/smartlambda/identity/GitHubIdentityProviderTest.java
+++ src/test/java/edu/teco/smartlambda/identity/GitHubIdentityProviderTest.java
@@ -6,6 +6,7 @@ import org.hibernate.Transaction;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -18,15 +19,16 @@ import static org.torpedoquery.jpa.Torpedo.where;
  * Created on 07.03.17.
  */
 public class GitHubIdentityProviderTest {
-	final String GITHUB_USERNAME = "umacs4hj2z";
-	final String GITHUB_TOKEN = "209318dc0479214916b0b56c4d7b69755f8f8465";
+	//This shall be a personal GitHub authentication token from an existing GitHub-account. It's name is verified in gitHubAuthentication()
+	final String GITHUB_USERNAME = "";
+	final String GITHUB_TOKEN = "";
 	String token;
 	User user;
 	@Before
 	public void buildUp() {
 		Application.getInstance().getSessionFactory().getCurrentSession().beginTransaction();
 		final Map<String, String> params = new HashMap<>();
-		//This is a personal GitHub authentication token from an existing GitHub-account. It's name is verified in gitHubAuthentication()
+
 		this.token = GITHUB_TOKEN;
 		params.put("accessToken", this.token);
 		this.user = new GitHubIdentityProvider().register(params).getLeft();
@@ -38,15 +40,18 @@ public class GitHubIdentityProviderTest {
 		if (transaction.isActive()) transaction.rollback();
 	}
 	
+	
+	@Ignore
 	@Test
-	public void gitHubCredentialPersistence() {
+	public void gitHubCredentialPersistence() throws Exception{
 		final GitHubCredential query = from(GitHubCredential.class);
 		where(query.getAccessToken()).eq(this.token);
 		Assert.assertFalse(select(query).list(Application.getInstance().getSessionFactory().getCurrentSession()).isEmpty());
 	}
 	
+	@Ignore
 	@Test
-	public void gitHubAuthentication() {
+	public void gitHubAuthentication() throws Exception{
 		Assert.assertTrue(this.user.getName().equals(GITHUB_USERNAME));
 	}
 }
