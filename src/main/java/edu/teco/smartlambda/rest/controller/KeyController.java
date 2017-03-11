daa1a7d0e444f3ec75c3886d462591966df6b76e
Yussuf Khalil <uyebg@student.kit.edu>
2017-03-11 16:48:50 +0100

    Add javadoc for KeyController

--- src/main/java/edu/teco/smartlambda/rest/controller/KeyController.java
+++ src/main/java/edu/teco/smartlambda/rest/controller/KeyController.java
@@ -10,15 +10,44 @@ import edu.teco.smartlambda.rest.exception.KeyNotFoundException;
 import spark.Request;
 import spark.Response;
 
+/**
+ * Provides REST calls related to lifecycle handling of access keys.
+ */
 public class KeyController {
-	public static Object createKey(final Request request, final Response response)
-			throws InsufficientPermissionsException, DuplicateKeyException {
+	/**
+	 * <code><b>PUT</b> /key/<i>:name</i></code>
+	 * <p>
+	 * Generates a new access key.
+	 * </p>
+	 * <p>
+	 * The request must contain a new, unused key name. No body parameters are required. Responds with a JSON-encoded string that
+	 * represents the generated key.
+	 * </p>
+	 *
+	 * @throws NotAuthenticatedException        <b>401</b> Thrown when user is not properly authenticated
+	 * @throws InsufficientPermissionsException <b>403</b> Thrown when the currently authenticated key is not permitted to create new keys
+	 * @throws DuplicateKeyException            <b>409</b> Thrown in case a key with the specified name already exists
+	 */
+	public static Object createKey(final Request request, final Response response) {
 		final User user = AuthenticationService.getInstance().getAuthenticatedUser().orElseThrow(NotAuthenticatedException::new);
 		
 		response.status(201);
 		return user.createKey(request.params(":name")).getRight();
 	}
 	
+	/**
+	 * <code><b>DELETE</b> /key/<i>:name</i></code>
+	 * <p>
+	 * Deletes an existing access key.
+	 * </p>
+	 * <p>
+	 * The request must contain the name of an existing key. No body parameters are required. Responds with an empty JSON object.
+	 * </p>
+	 *
+	 * @throws NotAuthenticatedException        <b>401</b> Thrown when user is not properly authenticated
+	 * @throws InsufficientPermissionsException <b>403</b> Thrown when the currently authenticated key is permitted to delete other keys
+	 * @throws KeyNotFoundException             <b>404</b> Thrown when no key with the specified key does not exist
+	 */
 	public static Object deleteKey(final Request request, final Response response) {
 		final User user = AuthenticationService.getInstance().getAuthenticatedUser().orElseThrow(NotAuthenticatedException::new);
 		final Key  key  = user.getKeyByName(request.params(":name")).orElseThrow(() -> new KeyNotFoundException(request.params(":name")));
