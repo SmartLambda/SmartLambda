2d1f5fdd494fa359f63d017e69f03e9638d6f12a
Jonathan Böttcher <utefh@student.kit.edu>
2017-03-17 18:52:07 +0100

    Updated JavaDoc for Exception Classes in Authentication Package

--- src/main/java/edu/teco/smartlambda/authentication/InsufficientPermissionsException.java
+++ src/main/java/edu/teco/smartlambda/authentication/InsufficientPermissionsException.java
@@ -1,7 +1,7 @@
 package edu.teco.smartlambda.authentication;
 
 /**
- *
+ * Is thrown when the authenticated Key has no permission for the actual operation.
  */
 public class InsufficientPermissionsException extends RuntimeException{
 	public InsufficientPermissionsException() {
