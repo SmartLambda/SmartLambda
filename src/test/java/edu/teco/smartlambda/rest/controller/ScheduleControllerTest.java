a4b1a2e47d0b26c3116b30eed57cebc531467493
Yussuf Khalil <uyebg@student.kit.edu>
2017-03-21 18:52:14 +0100

    Implement test case for ScheduleController#getScheduledEvents()

--- src/test/java/edu/teco/smartlambda/rest/controller/ScheduleControllerTest.java
+++ src/test/java/edu/teco/smartlambda/rest/controller/ScheduleControllerTest.java
@@ -1,6 +1,5 @@
 package edu.teco.smartlambda.rest.controller;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import edu.teco.smartlambda.authentication.AuthenticationService;
@@ -24,9 +23,17 @@ import spark.Request;
 import spark.Response;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
@@ -55,9 +62,9 @@ public class ScheduleControllerTest {
 	@Data
 	@AllArgsConstructor
 	private static class ScheduleResponse {
-		private final String     name;
-		private final String     calendar;
-		private final ObjectNode parameters;
+		private final String name;
+		private final String calendar;
+		private final String parameters;
 	}
 	
 	@Before
@@ -128,6 +135,27 @@ public class ScheduleControllerTest {
 		verify(response).status(200);
 	}
 	
+	private ScheduleResponse validateScheduleResponseObject(final Object object) throws Exception {
+		final Field name       = object.getClass().getDeclaredField("name");
+		final Field parameters = object.getClass().getDeclaredField("parameters");
+		final Field calendar   = object.getClass().getDeclaredField("calendar");
+		
+		assertEquals(3, object.getClass().getDeclaredFields().length);
+		
+		assertNotNull(name);
+		assertSame(String.class, name.getType());
+		assertNotNull(parameters);
+		assertSame(String.class, parameters.getType());
+		assertNotNull(calendar);
+		assertSame(String.class, calendar.getType());
+		
+		name.setAccessible(true);
+		parameters.setAccessible(true);
+		calendar.setAccessible(true);
+		
+		return new ScheduleResponse((String) name.get(object), (String) calendar.get(object), (String) parameters.get(object));
+	}
+	
 	@Test
 	public void readSchedule() throws Exception {
 		final Request    request    = mock(Request.class);
@@ -145,19 +173,11 @@ public class ScheduleControllerTest {
 		when(event.getParameters()).thenReturn(new Gson().toJson(jsonObject));
 		when(this.testLambda.getScheduledEvent(TEST_SCHEDULE_NAME)).thenReturn(Optional.of(event));
 		
-		final Object object     = ScheduleController.readSchedule(request, response);
-		final Field  name       = object.getClass().getDeclaredField("name");
-		final Field  parameters = object.getClass().getDeclaredField("parameters");
-		final Field  calendar   = object.getClass().getDeclaredField("calendar");
+		final ScheduleResponse scheduleResponse = this.validateScheduleResponseObject(ScheduleController.readSchedule(request, response));
 		
-		assertEquals(3, object.getClass().getDeclaredFields().length);
-		name.setAccessible(true);
-		parameters.setAccessible(true);
-		calendar.setAccessible(true);
-		
-		assertEquals(TEST_SCHEDULE_NAME, name.get(object));
-		assertEquals(TEST_CRON_EXPRESSION, calendar.get(object));
-		assertEquals(new Gson().toJson(jsonObject), parameters.get(object));
+		assertEquals(TEST_SCHEDULE_NAME, scheduleResponse.getName());
+		assertEquals(TEST_CRON_EXPRESSION, scheduleResponse.getCalendar());
+		assertEquals(new Gson().toJson(jsonObject), scheduleResponse.getParameters());
 		verify(response).status(200);
 	}
 	
@@ -182,6 +202,66 @@ public class ScheduleControllerTest {
 	
 	@Test
 	public void getScheduledEvents() throws Exception {
+		final Request    request    = mock(Request.class);
+		final Response   response   = mock(Response.class);
+		final JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty(TEST_PARAMETER_NAME, TEST_PARAMETER_VALUE);
 		
+		when(request.params(":user")).thenReturn(TEST_USER_NAME);
+		when(request.params(":name")).thenReturn(TEST_LAMBDA_NAME);
+		
+		final List<Event> events = new LinkedList<>();
+		Event             event  = mock(Event.class);
+		
+		when(event.getName()).thenReturn(TEST_SCHEDULE_NAME);
+		when(event.getParameters()).thenReturn(null);
+		when(event.getCronExpression()).thenReturn(TEST_CRON_EXPRESSION);
+		
+		events.add(event);
+		
+		event = mock(Event.class);
+		when(event.getName()).thenReturn(TEST_SCHEDULE_NAME + "2");
+		when(event.getParameters()).thenReturn(new Gson().toJson(jsonObject));
+		when(event.getCronExpression()).thenReturn("0 * * * *");
+		
+		events.add(event);
+		
+		when(this.testLambda.getScheduledEvents()).thenReturn(events);
+		
+		final Object result = ScheduleController.getScheduledEvents(request, response);
+		assertTrue(result instanceof Collection);
+		final Collection collection = (Collection) result;
+		assertEquals(2, collection.size());
+		final Iterator iterator = collection.iterator();
+		
+		final String nextName;
+		final String nextParameters;
+		final String nextCronExpression;
+		
+		ScheduleResponse scheduleResponse = this.validateScheduleResponseObject(iterator.next());
+		
+		if (scheduleResponse.getName().equals(TEST_SCHEDULE_NAME + "2")) {
+			nextName = TEST_SCHEDULE_NAME;
+			nextParameters = null;
+			nextCronExpression = TEST_CRON_EXPRESSION;
+			
+			assertEquals(TEST_SCHEDULE_NAME + "2", scheduleResponse.getName());
+			assertEquals(new Gson().toJson(jsonObject), scheduleResponse.getParameters());
+			assertEquals("0 * * * *", scheduleResponse.getCalendar());
+		} else {
+			nextName = TEST_SCHEDULE_NAME + "2";
+			nextParameters = new Gson().toJson(jsonObject);
+			nextCronExpression = "0 * * * *";
+			
+			assertEquals(TEST_SCHEDULE_NAME, scheduleResponse.getName());
+			assertNull(scheduleResponse.getParameters());
+			assertEquals(TEST_CRON_EXPRESSION, scheduleResponse.getCalendar());
+		}
+		
+		scheduleResponse = this.validateScheduleResponseObject(iterator.next());
+		
+		assertEquals(nextName, scheduleResponse.getName());
+		assertEquals(nextParameters, scheduleResponse.getParameters());
+		assertEquals(nextCronExpression, scheduleResponse.getCalendar());
 	}
 }
