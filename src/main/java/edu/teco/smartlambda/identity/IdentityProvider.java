3e9ed3b117e8940e5f37dd0f36544e1f62f3baf9
Johannes Hengstler <ubezl@student.kit.edu>
2017-03-24 19:40:23 +0100

    Added JavaDoc to IdentityProvider interface

--- src/main/java/edu/teco/smartlambda/identity/IdentityProvider.java
+++ src/main/java/edu/teco/smartlambda/identity/IdentityProvider.java
@@ -6,9 +6,23 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.Map;
 
 /**
- * Created on 28.02.17.
+ * Provide any form of external identity verification
  */
 public interface IdentityProvider {
-	Pair<User, String> register(Map<String, String> parameters) throws IdentityException;
-	String getName();
+	
+	/**
+	 * Registers a user with an external identification at the SmartLambda system
+	 *
+	 * @param parameters A set of parameters required for identity verification. Content depends on verification implementation
+	 *
+	 * @return A pair of a {@link User} and the name this user is identified with
+	 *
+	 * @throws IdentityException if the verification process fails
+	 */
+	public Pair<User, String> register(Map<String, String> parameters) throws IdentityException;
+	
+	/**
+	 * @return the provider service's name
+	 */
+	public String getName();
 }
