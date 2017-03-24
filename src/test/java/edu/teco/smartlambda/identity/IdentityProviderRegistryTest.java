29591bf2c5f3ce2151a0633e8ac874066852919f
Johannes Hengstler <ubezl@student.kit.edu>
2017-03-24 19:33:43 +0100

    Implemented Id-Provider registry tests

--- src/test/java/edu/teco/smartlambda/identity/IdentityProviderRegistryTest.java
+++ src/test/java/edu/teco/smartlambda/identity/IdentityProviderRegistryTest.java
@@ -1,31 +1,49 @@
 package edu.teco.smartlambda.identity;
 
-import org.apache.commons.configuration2.ImmutableConfiguration;
-import org.junit.Assert;
+import edu.teco.smartlambda.configuration.ConfigurationService;
+import org.apache.commons.configuration2.Configuration;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-/**
- * Created on 22.03.17.
- */
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(ImmutableConfiguration.class)
+@PrepareForTest(ConfigurationService.class)
 public class IdentityProviderRegistryTest {
 	
-	@Test
-	public void getInstanceNotNull() throws Exception {
-		Assert.assertNotNull(IdentityProviderRegistry.getInstance());
+	@Before
+	public void setup() {
+		mockStatic(ConfigurationService.class);
+		Mockito.when(ConfigurationService.getInstance()).thenReturn(mock(ConfigurationService.class));
+		
+		Mockito.when(ConfigurationService.getInstance().getConfiguration()).thenReturn(mock(Configuration.class));
+		Mockito.when(ConfigurationService.getInstance().getConfiguration().getList(eq(String.class), anyString()))
+				.thenReturn(Collections.singletonList(GitHubIdentityProvider.class.getName()));
 	}
 	
 	@Test
 	public void getInstanceReturnsSingleton() throws Exception {
-		final IdentityProviderRegistry irFirst = IdentityProviderRegistry.getInstance();
-		assert irFirst!=null;
-		final IdentityProviderRegistry irSecond = IdentityProviderRegistry.getInstance();
-		assert irSecond!=null;
-		Assert.assertSame(irFirst, irSecond);
+		assertNotNull(IdentityProviderRegistry.getInstance());
+		assertSame(IdentityProviderRegistry.getInstance(), IdentityProviderRegistry.getInstance());
 	}
-
+	
+	@Test
+	public void getIdentityProviderByName() throws Exception {
+		assertTrue(IdentityProviderRegistry.getInstance().getIdentityProviderByName("github").isPresent());
+		assertEquals(IdentityProviderRegistry.getInstance().getIdentityProviderByName("github").get().getClass(),
+				GitHubIdentityProvider.class);
+	}
 }
