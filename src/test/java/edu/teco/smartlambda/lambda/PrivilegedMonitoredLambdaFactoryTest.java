5f34f5521f9ba1046f08a268476b80f5db0cbf5c
Johannes Hengstler <ubezl@student.kit.edu>
2017-03-27 19:19:47 +0200

    Mocked RuntimeRegistry in lambda creation test to not depend on configuration

--- src/test/java/edu/teco/smartlambda/lambda/PrivilegedMonitoredLambdaFactoryTest.java
+++ src/test/java/edu/teco/smartlambda/lambda/PrivilegedMonitoredLambdaFactoryTest.java
@@ -1,15 +1,24 @@
 package edu.teco.smartlambda.lambda;
 
+import edu.teco.smartlambda.runtime.RuntimeRegistry;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 /**
  *
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(RuntimeRegistry.class)
 public class PrivilegedMonitoredLambdaFactoryTest {
 	
 	private PrivilegedMonitoredLambdaFactory factory;
@@ -26,6 +35,9 @@ public class PrivilegedMonitoredLambdaFactoryTest {
 		// not a prototype
 		assertNotNull(emptyLambda);
 		assertNotSame(LambdaFacade.getInstance().getFactory().createLambda(), emptyLambda);
+		
+		mockStatic(RuntimeRegistry.class);
+		when(RuntimeRegistry.getInstance()).thenReturn(mock(RuntimeRegistry.class));
 		
 		// is it empty
 		assertNull(emptyLambda.getOwner());
