0143b61ff18e80e252eacafbf3aef9c832de0665
Jonathan Böttcher <utefh@student.kit.edu>
2017-03-27 20:09:28 +0200

    Changed GitHubCredentialTest setUp method to mock User, GitHubCredential, Torpedo, Query, SessionFactory, Application classes.

--- src/test/java/edu/teco/smartlambda/identity/GitHubIdentityProviderTest.java
+++ src/test/java/edu/teco/smartlambda/identity/GitHubIdentityProviderTest.java
@@ -1,50 +1,109 @@
 package edu.teco.smartlambda.identity;
 
+import edu.teco.smartlambda.Application;
 import edu.teco.smartlambda.authentication.entities.User;
 import edu.teco.smartlambda.utility.TestUtility;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.torpedoquery.jpa.OnGoingStringCondition;
+import org.torpedoquery.jpa.Query;
+import org.torpedoquery.jpa.Torpedo;
 
 import java.util.HashMap;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 /**
  * Created on 22.03.17.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(User.class)
+@PrepareForTest({User.class, Torpedo.class, Application.class})
 public class GitHubIdentityProviderTest {
 	
-	private String answer;
+	private String                 createUserAnswer;
+	private Class                  torpedoFromAnswer;
+	private String                 torpedoWhereAnswer;
+	private Object                 sessionSaveAnswer;
+	private Session                session;
+	private OnGoingStringCondition onGoingStringCondition;
+	private GitHubCredential       gitHubCredential;
+	
+	private GitHubIdentityProvider gitHubIdentityProvider;
 	
 	@Before
 	public void setUp() throws Exception {
+		this.gitHubIdentityProvider = new GitHubIdentityProvider();
+		
+		//Don't create a new User but check the supplied name
 		PowerMockito.mockStatic(User.class);
 		PowerMockito.when(User.createUser(anyString())).thenAnswer(invocation -> {
-			this.answer = (String) invocation.getArguments()[0];
+			this.createUserAnswer = (String) invocation.getArguments()[0];
 			return null;
 		});
+		
+		//Catch and test Torpedoquery invocations
+		GitHubCredential gitHubCredential = PowerMockito.mock(GitHubCredential.class);
+		PowerMockito.when(gitHubCredential.getAccessToken()).thenReturn("mockedAccessToken");
+		
+		PowerMockito.mockStatic(Torpedo.class);
+		PowerMockito.when(Torpedo.from(GitHubCredential.class)).thenReturn(gitHubCredential);
+		PowerMockito.when(Torpedo.where(anyString())).thenAnswer(invocation -> {
+			this.torpedoWhereAnswer = (String) invocation.getArguments()[0];
+			return PowerMockito.mock(OnGoingStringCondition.class);
+		});
+		final Query query = PowerMockito.mock(Query.class);
+		PowerMockito.when(Torpedo.select(any(GitHubCredential.class))).thenReturn(query);
+		PowerMockito.when(query.get(any())).thenReturn(Optional.empty());
+		
+		//Mocking Session class and their creation
+		final SessionFactory sessionFactory = Mockito.mock(SessionFactory.class);
+		this.session = Mockito.mock(Session.class);
+		final Application app = Mockito.mock(Application.class);
+		PowerMockito.mockStatic(Application.class);
+		PowerMockito.when(Application.getInstance()).thenReturn(app);
+		PowerMockito.when(Application.getInstance().getSessionFactory()).thenReturn(sessionFactory);
+		PowerMockito.when(Application.getInstance().getSessionFactory().getCurrentSession()).thenReturn(this.session);
 	}
 	
-	@Test (expected = IdentitySyntaxException.class)
+	@Test(expected = IdentitySyntaxException.class)
 	public void testNullParameter() {
-		new GitHubIdentityProvider().register(null);
+		this.gitHubIdentityProvider.register(null);
 	}
 	
-	@Test (expected = IdentitySyntaxException.class)
+	@Test(expected = IdentitySyntaxException.class)
 	public void testEmptyParameter() {
-		new GitHubIdentityProvider().register(new HashMap<>());
+		this.gitHubIdentityProvider.register(new HashMap<>());
 	}
 	
 	@Test
-	public void testGettersAndSetters() throws Exception{
-		TestUtility.testGettersAndSetters(new GitHubIdentityProvider());
+	public void testGettersAndSetters() throws Exception {
+		TestUtility.testGettersAndSetters(this.gitHubIdentityProvider);
 	}
 	
+	@Ignore
+	@Test
+	public void alreadyExistingAccessToken() throws Exception {
+		//TODO
+	}
 	
+	@Test
+	public void gitHubCredentialCreation() throws Exception {
+		//TODO
+	}
+	
+	@Ignore
+	@Test
+	public void gitHubRequestTest() throws Exception {
+		//TODO
+	}
 }
