cc866e5d0ad7511a2942bc43e560620746c2f954
Johannes Hengstler <ubezl@student.kit.edu>
2017-03-27 19:47:20 +0200

    Removed getter/setter cover utility (bad practise)

--- src/test/java/edu/teco/smartlambda/utility/TestUtility.java
+++ src/test/java/edu/teco/smartlambda/utility/TestUtility.java
@@ -2,7 +2,6 @@ package edu.teco.smartlambda.utility;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 /**
  * A utility class for test utilities.
@@ -49,27 +48,6 @@ public final class TestUtility {
 		for (final Constructor<?> constructor : clazz.getDeclaredConstructors()) {
 			constructor.setAccessible(true);
 			constructor.newInstance(new Object[constructor.getParameterCount()]);
-		}
-	}
-	
-	/**
-	 * Test all getters and setters of an object instance. The instance should not be used anymore afterwards, since it calls every
-	 * setter with 'null' which may lead to an unstable state. Furthermore, this method shall not be used, if methods exist in a class
-	 * which are called like simple getters and setters (set***(...), get***()), that are no simple getters and setters.
-	 *
-	 * @param o an object instance of the class whose methods shall be tested
-	 *
-	 * @throws InvocationTargetException If any exception rises in the getters/setters
-	 * @throws IllegalAccessException    this exception is highly unexpected, because the constructor is made accessible by reflection
-	 */
-	public static void testGettersAndSetters(final Object o) throws InvocationTargetException, IllegalAccessException {
-		for (final Method method : o.getClass().getDeclaredMethods()) {
-			method.setAccessible(true);
-			if (method.getName().startsWith("get") && method.getParameterCount() == 0 && !method.getReturnType().equals(Void.TYPE)) {
-				assert method.invoke(o) != null;
-			} else if (method.getName().startsWith("set") && method.getParameterCount() == 1 && method.getReturnType().equals(Void.TYPE)) {
-				method.invoke(o, new Object[] {null});
-			}
 		}
 	}
 }
