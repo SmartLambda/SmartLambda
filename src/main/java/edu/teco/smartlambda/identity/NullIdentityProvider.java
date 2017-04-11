ee9f622d204c5d763ee398f85f9a64438a79855c
Jonathan Böttcher <utefh@student.kit.edu>
2017-04-11 16:41:47 +0200

    Added username check to ensure full REST-call compatibility

--- src/main/java/edu/teco/smartlambda/identity/NullIdentityProvider.java
+++ src/main/java/edu/teco/smartlambda/identity/NullIdentityProvider.java
@@ -20,6 +20,7 @@ public class NullIdentityProvider implements IdentityProvider{
 		if (name == null) {
 			throw new IdentitySyntaxException();
 		}
+		if (name.toLowerCase().startsWith("key/")) throw new IdentitySyntaxException("Username starting with \"key/\" not supported");
 		//User user = User.createUser(NAME).getLeft();
 		//Application.getInstance().getSessionFactory().getCurrentSession().save(user);
 		//return user;
